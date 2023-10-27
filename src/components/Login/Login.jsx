b796d4d42f0afe8c77bf56b02cceae34f5bb9392
DressedHuman <dressed.human@gmail.com>
2023-10-28 02:37:17 +0600

    deployed to surge

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -27,13 +27,13 @@ const Login = () => {
                                 <legend className="text-left mx-7 px-2">Sign In</legend>
                                 <input className="border border-black rounded-lg py-3 px-5" placeholder="Type your email.." type="email" ref={email} name="email" />
                                 <input className="border border-black rounded-lg py-3 pl-5" placeholder="Type your password.." type="password" ref={password} name="password" />
-                                <button onClick={handleSubmit} className="bg-[green] rounded-lg py-3 px-4 text-white font-semibold text-xl w-1/3 mx-auto" type="submit">Submit</button>
+                                <button onClick={handleSubmit} className="bg-[green] text-white px-3 py-2 rounded-lg active:text-[17px] transition-all duration-[175ms] w-auto mx-auto text-lg font-medium" type="submit">Submit</button>
                             </fieldset>
                         </form>
                         <div className="mt-7">
                             <p className="m-5">or</p>
                             <div className="flex justify-center items-center gap-4">
-                                <button onClick={() => handleSignIn(providerGoogle)} className="bg-[green] text-white px-3 py-2 rounded-lg active:text-[15px] transition-all duration-[175ms]">Google Login</button>
+                                <button onClick={() => handleSignIn(providerGoogle)} className="bg-[green] text-white px-3 py-2 rounded-lg active:text-[17px] transition-all duration-[175ms] text-lg font-medium">Google Login</button>
                             </div>
                         </div>
                     </div>
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -22,11 +22,11 @@ const SignUp = () => {
     return (
         <div>
             <form className="text-black">
-                <fieldset className="grid grid-cols-1 gap-3 justify-center items-center px-7 py-5 border-[3px] border-black rounded-lg w-1/3 mx-auto">
+                <fieldset className="grid grid-cols-1 gap-3 justify-center items-center px-7 py-5 border-2 border-black border-opacity-[12%] rounded-lg w-1/3 mx-auto">
                     <legend className="text-left mx-7 px-2">Create New Account</legend>
                     <input className="border border-black rounded-lg py-3 px-5" placeholder="Type your email.." type="email" ref={email} name="email" />
                     <input className="border border-black rounded-lg py-3 pl-5" placeholder="Type your password.." type="password" ref={password} name="password" />
-                    <button onClick={handleSubmit} className="bg-[green] rounded-lg py-3 px-4 text-white font-semibold text-xl w-1/3 mx-auto" type="submit">Submit</button>
+                    <button onClick={handleSubmit} className="bg-[green] text-white px-3 py-2 rounded-lg active:text-[17px] transition-all duration-[175ms] w-auto mx-auto text-lg font-medium" type="submit">Submit</button>
                 </fieldset>
             </form>
         </div>
